91b8b21b7db908f5466eb2508a0cdcc60e5c4796
Anubhav Shrivastava <anubhaviam@gmail.com>
2018-04-16 21:57:31 +0530

    Tenth Chapter[Password Reset Email] complete

--- config.py
+++ config.py
@@ -9,5 +9,12 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-	
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get('MAIL_PORT')
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
+
+	ADMINS = ['anubhaviam@gmail.com']
